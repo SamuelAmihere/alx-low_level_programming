b32bcfb92bd99b3960455b0cbe5bc956c33c4ac5
SamuelAmihere <amisam2000@gmail.com>
2023-03-16 15:32:56 -0700

    prints using fprintf

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,8 +7,10 @@
  */
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\" -
-			 Dora Korpar, 2015-10-19");
+	char *myStr = "and that piece of art is useful\"
+		- Dora Korpar, 2015-10-19";
+
+	fprintf(stderr, myStr);
 
 	return (1);
 }
