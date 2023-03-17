c6ff49e2edbf37689eef83eb279ff8de5fa87aa0
SamuelAmihere <amisam2000@gmail.com>
2023-03-17 07:47:45 -0700

    test for 0, >5, and <6

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -16,8 +16,9 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
 
-	int lastDigit = n % 10;
+	int lastDigit;
 
+	lastDigit = n % 10;
 	if (lastDigit > 5)
 	{
 		printf("Last digit of %d is %d and is greater than 5\n", n,
