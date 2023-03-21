88d7ba324de17fdef1cc7294e39bce553a72478f
SamuelAmihere <amisam2000@gmail.com>
2023-03-21 11:43:08 -0700

    First commit

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+
+/**
+ * _abs - computes absolutes
+ *
+ * @n: a number whose absolute to be computed
+ *
+ * Return: Absolute number
+ */
+int _abs(int n)
+{
+	return (abs(n));
+}
