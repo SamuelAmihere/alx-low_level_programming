307567c298f0c481b1822aa2afea4681b1564111
SamuelAmihere <amisam2000@gmail.com>
2023-03-21 16:46:33 -0700

    First commit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+/**
+ * add - adds two numbers
+ *
+ * @n: first number to add
+ *
+ * @m: second number to add
+ *
+ * Return: sum of two numbers
+ */
+int add(int n, int m)
+{
+	int ans;
+
+	ans = n + m;
+	return (ans);
+}
