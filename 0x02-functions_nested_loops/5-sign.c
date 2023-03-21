f86a245db36a956cc2d287441074fcc2eaa6dc3b
SamuelAmihere <amisam2000@gmail.com>
2023-03-21 11:28:31 -0700

    First commit

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <ctype.h>
+#include "main.h"
+
+/**
+ * print_sign - checks is a character is lower
+ *
+ * @n: a number whose sign is to be printed
+ *
+ * Return: Always 1 n is positive, -1 n is negative 0 otherwise
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	_putchar('0');
+	return (0);
+}
