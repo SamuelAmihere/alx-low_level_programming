dd40c8647d5986549a044951e9f34d75efc15386
SamuelAmihere <amisam2000@gmail.com>
2023-03-21 21:42:52 -0700

    Add newline

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *
+ * Return: Always 0 on Success
+ */
+int main(void)
+{
+	int i;
+	int sum;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if ((i % 3) == 0 || (i % 5) == 0)
+		{
+			sum += i;
+		}
+	}
+	printf("%d", sum);
+	putchar('\n');
+}
