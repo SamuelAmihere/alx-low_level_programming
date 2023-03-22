e7e4309c436013cd2beabb989303e19ff3a703a1
SamuelAmihere <amisam2000@gmail.com>
2023-03-22 03:42:42 -0700

    add newline

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -19,9 +19,9 @@ void print_times_table(int n)
 		return;
 	}
 
-	for (i = 0; i < n; i++)
+	for (i = 0; i <= n; i++)
 	{
-		for (j = 0; j < n; j++)
+		for (j = 0; j <= n; j++)
 		{
 			ans = i * j;
 			if (ans == 0)
