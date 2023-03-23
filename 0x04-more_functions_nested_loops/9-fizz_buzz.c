601bfb5cb29d707eb4129c578f8c895caac49741
SamuelAmihere <amisam2000@gmail.com>
2023-03-23 12:02:01 -0700

    Change condition

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,17 +13,16 @@ int main(void)
 	for (i = 1; i <= 100; i++)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
-			printf("FizzBuzz");
+			printf(" FizzBuzz");
 		else if (i % 3 == 0)
-			printf("Fizz");
+			printf(" Fizz");
 		else if (i % 5 == 0)
-			printf("Buzz");
-		else
+			printf(" Buzz");
+		else if (i == 1)
 			printf("%d", i);
-		if (i == 100)
-			_putchar('\n');
 		else
-			_putchar(' ');
+			printf(" %d", i);
 	}
+	_putchar('\n');
 	return (0);
 }
