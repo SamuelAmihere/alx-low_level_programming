e0aead4b5580834eb21775b1d3f83757386a2c70
SamuelAmihere <amisam2000@gmail.com>
2023-03-23 07:15:18 -0700

    First commit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * mil - multiplies two digits
+ *
+ * @a: a digit
+ *
+ * @b: a digit
+ *
+ * Return: integer
+ */
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+	return (result);
+}
