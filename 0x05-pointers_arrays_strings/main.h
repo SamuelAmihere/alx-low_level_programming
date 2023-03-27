d8877b2d48c0835c578c75caa6b8b15b8ad7d019
SamuelAmihere <amisam2000@gmail.com>
2023-03-27 06:52:12 -0700

    add rev_string

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -4,6 +4,7 @@ void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
 void print_rev(char *s);
+void rev_string(char *s);
 int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
