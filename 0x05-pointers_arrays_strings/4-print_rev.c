90c4b51d16e28cace98549db705890f01f599c23
SamuelAmihere <amisam2000@gmail.com>
2023-03-27 05:25:50 -0700

    use for-loop

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -17,24 +17,3 @@ void print_rev(char *s)
 
 	_putchar('\n');
 }
-
-/**
- * _strlen - determine length of string
- *
- * @s: pointerb to string array
- *
- * Return: length of array
- */
-
-int _strlen(char *s)
-{
-	int len = 0;
-
-	while (*s != '\0')
-	{
-		len++;
-		s++;
-	}
-
-	return (len);
-}
