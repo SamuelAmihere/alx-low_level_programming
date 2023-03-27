47962372a811b419c237ba4542d004a169076044
SamuelAmihere <amisam2000@gmail.com>
2023-03-27 03:44:55 -0700

    Edit comment

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,12 @@
 #include "main.h"
 
 /**
-*reset_to_98 - updates a value through pointer
-*
-*@n: a pointer of int
-*
-*Return: void
+ * reset_to_98 - updates value of an address
+ *
+ * @n: pointer
+ *
+ * Return - void
+ */
 
 void reset_to_98(int *n)
 {
