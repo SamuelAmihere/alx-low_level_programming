cf201c947ad3e286156e2f85c9093d6a585fbb76
SamuelAmihere <amisam2000@gmail.com>
2023-03-27 05:05:14 -0700

    use for-loop

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -12,11 +12,8 @@ void print_rev(char *s)
 {
 	int i;
 
-	i = -1;
-	while (s[i] != '\0')
-	{
-		_putchar(s[i]);
-		i--;
-	}
+	for (i = _strlen(*s); i > 0; i--)
+		_putchar('\n');
+
 	_putchar('\n');
 }
