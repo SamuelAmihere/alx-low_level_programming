b994975a56d204c4f1587c55d45569c9dbdf9479
SamuelAmihere <amisam2000@gmail.com>
2023-03-27 19:31:24 -0700

    first commit

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -16,8 +16,13 @@ void puts_half(char *str)
 	while (str[i] != '\0')
 		i++;
 
-	j = i / 2;
+	if (i % 2 == 0)
+		j = i / 2;
+	else
+		j = (i + 1) / 2;
 	for (; j <= i; j++)
 		_putchar(str[j]);
+
+	_putchar('\n');
 
 }
