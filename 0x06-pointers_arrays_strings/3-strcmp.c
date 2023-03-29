e7e96b51e6492803b2f0805c00b8ddb182c85a9b
SamuelAmihere <amisam2000@gmail.com>
2023-03-29 11:22:52 -0700

    first commit

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strcmp - compare two strings
+ *
+ * @s1: first string
+ *
+ * @s2: second string
+ *
+ * Return: integer after comparing s1 to s2
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int result = 15;
+
+	while (*s1 != '\0' && *s2 != '\0' && *s1 == *s2)
+	{
+		s1++;
+		s2++;
+	}
+
+	return ((*s1 == *s2) ? 0 : (*s1 < *s2) ? -result : result);
+}
