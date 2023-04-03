fe51d45f5a46f6f8d68c4b83ade8f61f0bb04168
SamuelAmihere <amisam2000@gmail.com>
2023-04-03 12:56:55 -0700

    edit

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,16 +11,30 @@
  */
 char *_strchr(char *s, char c)
 {
-	while (*s != '\0')
-	{
-		if (*s != c)
-			continue;
-		else
-			return (s);
-		s++;
-	}
+	int string_length(const char *str);
 
+	for (int i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+			return (&s[i]);
+	}
 	if (c == '\0')
-		return (s);
+		return (&s[string_length(s)]);
 	return (NULL);
+}
+
+/**
+ * string_length - find length of string
+ * @str: string to find length
+ * Return: lenth of str
+ */
+int string_length(const char *str)
+{
+	int length = 0;
+
+	while (str[length] != '\0')
+	{
+		length++;
+	}
+	return (length);
 }
