a695d2f86b6563e505e4102123edf54e99523a74
SamuelAmihere <amisam2000@gmail.com>
2023-04-03 18:04:57 -0700

    edit

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -14,13 +14,13 @@ void print_diagsums(int *a, int size)
 {
 	int i, left_d = 0, right_d = 0;
 
-	for (i = 0; i < size * size; i += size + 1)
+	for (i = 0; i < (size * size); i += size + 1)
 	{
 		left_d += a[i];
 	}
-	for (i = size - 1; i < size * size - 1; i += size - 1)
+	for (i = (size - 1); i < (size * size - 1); i += size - 1)
 	{
 		right_d += a[i];
 	}
-	printf("%d, %d", left_d, right_d);
+	printf("%d, %d\n", left_d, right_d);
 }
