bccade05128155aa99257e0937e2f868dc4ab14c
SamuelAmihere <amisam2000@gmail.com>
2023-04-05 13:19:48 -0700

    int is_prime_number(int n)

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,31 @@
+#include "main.h"
+int get_sqrt(int n, int a, int b);
+/**
+ * _sqrt_recursion - find the natural square root of a number.
+ *
+ * @n: a number to find its root
+ *
+ * Return: natural square root of a number.
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	else
+		return get_sqrt(n, 0, 0);
+}
+
+/**
+ * get_sqrt - find the square root
+ * @n: the number to find the square root
+ * @a: lower bound of a search
+ * @b: upper bound of a search
+ * Return: the sqaure root
+ */
+int get_sqrt(int n, int a, int b)
+{
+	if ()
+	{
+	}
+}
