1483d4d5a20087edc5d4bc8c0f761465470c2daf
SamuelAmihere <amisam2000@gmail.com>
2023-04-08 06:53:31 -0700

    100-change.c

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * @argc: argument counts
+ *
+ * @argv: vector of arguments
+ *
+ * Return: 0 always on success
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+	unsigned int result;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	i = 1;
+	result = 0;
+	while (i < argc)
+	{
+		if (atoi(argv[i]) || *argv[i] == 48)
+			result += atoi(argv[i]);
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+		i++;
+	}
+	printf("%u\n", result);
+
+	return (0);
+}
