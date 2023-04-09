a66f97ef49c5820971430d09827fb7ae007dd877
SamuelAmihere <amisam2000@gmail.com>
2023-04-09 07:00:30 -0700

    100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
 
 /**
  * main - Entry point
@@ -22,30 +21,13 @@ int main(int argc, char **argv)
 		return (1);
 	}
 	num = atoi(argv[1]);
-	for (i = 0; i < 5; i++)
+	if (num > 0)
 	{
-		if (num < 0)
-		{
-			printf("0");
-			return (0);
-		}
-		if (num == 0)
-		{
-			printf("%d\n", result);
-			return (0);
-		}
-		if (denominations[i] <= num)
+		for (i = 0; i < 5 && num != 0; i++)
 		{
 			result += num / denominations[i];
-			num = abs(num - (denominations[i] * result));
-		}
-		for (j = 0; j < 5; j++)
-		{
-			if (num == denominations[j])
-			{
-				result += 1;
-				num = 0;
-			}
+			num %= denominations[i];
+
 		}
 	}
 	printf("%d\n", result);
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -1,2 +1,1 @@
 int _putchar(char c);
-int neg_or_nondigit(char *ptr);
