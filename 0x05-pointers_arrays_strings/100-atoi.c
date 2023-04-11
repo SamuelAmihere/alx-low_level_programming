b4915f21f87e56a9f219554d061ae76dbbb75d65
SamuelAmihere <amisam2000@gmail.com>
2023-04-10 18:04:05 -0700

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -10,26 +10,29 @@
 
 int _atoi(char *s)
 {
-	int i, h, p;
+	unsigned int count = 0, size = 0, oi = 0, pn = 1, m = 1, i;
 
-	h = 0, p = -1;
-	for (i = 0; s[i] != '\0'; i++)
+	while (*(s + count) != '\0')
 	{
-		if (s[i] == '-')
-			p *= -1;
-		if (s[i] > 47 && s[i] < 58)
+		if (size > 0 && (*(s + count) < '0' || *(s + count) > '9'))
+			break;
+
+		if (*(s + count) == '-')
+			pn *= -1;
+
+		if ((*(s + count) >= '0') && (*(s + count) <= '9'))
 		{
-			if (h < 0)
-				h = (h * 10) - (s[i] - '0');
-			else
-				h = (s[i] - '0') * -1;
-			if (s[i + 1] < 48 || s[i + 1] > 57)
-				break;
+			if (size > 0)
+				m *= 10;
+			size++;
 		}
+		count++;
 	}
 
-	if (p < 0)
-		h *= -1;
-
-	return (h);
+	for (i = count - size; i < count; i++)
+	{
+		oi = oi + ((*(s + i) - 48) * m);
+		m /= 10;
+	}
+	return (oi * pn);
 }
