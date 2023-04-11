f88aaa96fbe5dfca2643b6694c40ed86efaa9b37
SamuelAmihere <amisam2000@gmail.com>
2023-04-11 08:04:04 -0700

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,12 +14,19 @@
 char *create_array(unsigned int size, char c)
 {
 	char *ptr;
+	int i;
 
 	if (size == 0)
 		return (NULL);
 	ptr = malloc(sizeof(c) * size);
 	if (ptr != NULL)
+	{
+		for (i = 0; i < size; i++)
+		{
+			ptr[i] = c;
+		}
 		return (ptr);
+	}
 	else
 		return (NULL);
 
