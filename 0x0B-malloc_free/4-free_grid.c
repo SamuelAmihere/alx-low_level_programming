f7abfa4065214236fbd0e0a571610b30293a57f6
SamuelAmihere <amisam2000@gmail.com>
2023-04-11 21:08:08 -0700

    void free_grid(int **grid, int height)

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * free_grid - frees a 2 dimensional grid
+ *
+ * @grid: pointer being evaluated
+ * @height: int being evaluated
+ *
+ * Return: 0 on SUccess
+ */
+
+#include "main.h"
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+	free(grid);
+}
