b74205487279e540dab2648805064c3076617fe7
SamuelAmihere <amisam2000@gmail.com>
2023-04-13 13:51:45 -0700

    void *malloc_checked(unsigned int b)

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - allocates memory
+ *  memory
+ *
+ *  @b: unsigned integer type being allocated
+ *
+ *   Return: a pointer
+ */
+
+
+void *malloc_checked(unsigned int b)
+{
+	int *ptr;
+
+	ptr = malloc(b);
+
+	if (ptr != NULL)
+		return (ptr);
+	return (98);
+}
