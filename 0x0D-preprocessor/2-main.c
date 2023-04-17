342047cb82c0e18c77c25a15dc00d6210941a371
SamuelAmihere <amisam2000@gmail.com>
2023-04-17 13:12:34 -0700

    first commit

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+
+/**
+ * main - Entry Point
+ *
+ * @argc: numner of arguments
+ * @argv: pointer to arguments enntered
+ *
+ * Return: 0 on success
+ */
+int main(int argc, char ***argv)
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
