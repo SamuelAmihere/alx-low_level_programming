a1b1b42f89b09a24dff83f35804018a4b6b38d87
SamuelAmihere <amisam2000@gmail.com>
2023-04-17 22:12:42 -0700

    correct logic error

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -24,16 +24,16 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 
 	d->name = malloc(sizeof(char) * (_strlen(name)) + 1);
-	d->owner = malloc(sizeof(char) * (_strlen(owner)) + 1);
-
 	if (d->name == NULL)
 	{
-		free(d->name);
+		free(d->d);
 		return (NULL);
 	}
+
+	d->owner = malloc(sizeof(char) * (_strlen(owner)) + 1);
 	if (d->owner == NULL)
 	{
-		free(d->owner);
+		free(d->name);
 		free(d);
 		return (NULL);
 	}
