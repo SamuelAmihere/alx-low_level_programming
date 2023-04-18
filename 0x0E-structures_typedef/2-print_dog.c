405b75527d311d15b7c5bb252640c46a693df5c9
SamuelAmihere <amisam2000@gmail.com>
2023-04-17 19:26:13 -0700

    correct syn error

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -14,11 +14,13 @@ void print_dog(struct dog *d)
 {
 	if (d == NULL)
 		return;
+
 	if (d->name == NULL)
-		printf("name: (nil)");
+		d->name = "(nil)";
 	if (d->owner == NULL)
-		printf("owner: (nil)");
+		d->owner = "(nil)";
 
 	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name
-			, d->age, d->owner);
+				, d->age, d->owner);
+	}
 }
