2181cd58b3899072e47948655533d97a5f38999c
SamuelAmihere <amisam2000@gmail.com>
2023-04-17 20:16:14 -0700

    add 4-new_dog.c

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,83 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "dog.h"
+
+int _strlen(char *s);
+char *_strcpy(char *dest, char *src);
+
+/**
+ * new_dog - creates new dog
+ *
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: dog's owner
+ *
+ * Return: newly dog
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *d;
+
+	d = malloc(sizeof(dog_t));
+
+	if (d == NULL)
+		exit(1);
+
+	d->name = malloc(_strlen(name) + 1);
+	d->owner = malloc(_strlen(owner) + 1);
+
+	if (d->owner == NULL || d->name == NULL)
+	{
+		free(d);
+		return (NULL);
+	}
+
+	_strcpy(d->name, name);
+	_strcpy(d->owner, owner);
+
+	return (d);
+}
+
+/**
+ * _strlen - returns the length of a string.
+ *
+ * @s: input string.
+ *
+ * Return: length of a string.
+ */
+
+int _strlen(char *s)
+{
+	int count = 0;
+
+	while (*(s + count) != '\0')
+		count++;
+
+	return (count);
+}
+
+/**
+ * _strcpy - copies the string pointed to by src,
+ *
+ * including the terminating null byte, to the
+ * buffer pointed to by dest.
+ * @dest: destination.
+ * @src: source.
+ *
+ * Return: the pointer to dest.
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int count = 0;
+
+	while (count >= 0)
+	{
+		*(dest + count) = *(src + count);
+		if (*(src + count) == '\0')
+			break;
+		count++;
+	}
+
+	return (dest);
+}
