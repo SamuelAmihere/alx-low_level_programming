c4c35a5c3cc0ec2a4ae69dd72c150a98896f2138
SamuelAmihere <amisam2000@gmail.com>
2023-04-19 08:47:58 -0700

    correct error

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,3 @@
-#include <stdlib.h>
-
 #include "function_pointers.h"
 
 /**
@@ -12,5 +10,8 @@
 
 void print_name(char *name, void (*f)(char *))
 {
-	(*f)(name);
+	if (name == NULL || f == NULL)
+		return;
+
+	f(name);
 }
