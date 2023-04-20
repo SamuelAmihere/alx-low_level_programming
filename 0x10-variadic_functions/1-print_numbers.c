ea98e03112e6f927bde5c2ac4a7a930a6fb77ab6
SamuelAmihere <amisam2000@gmail.com>
2023-04-20 15:32:42 -0700

    correct error

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -23,12 +23,12 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 		if (separator)
 		{
 			printf("%d", va_arg(l, int));
-			if (i >= 0 && i < n - 1 && separator)
+			if ((i < n - 1) && separator)
 				printf("%s", separator);
 		}
 	}
 
 	va_end(l);
 
-	_putchar('\n');
+	printf("\n");
 }
