16444aa469e66fba27c424ff264f888fd16fc5e1
SamuelAmihere <amisam2000@gmail.com>
2023-04-27 18:34:24 -0700

    add file

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,18 @@
+global main
+extern printf
+
+section .text
+
+main:
+	mov esi, msg
+	mov edi, fmt
+	mov eax, 0
+	call printf
+
+	mov eax, 0
+	ret
+
+
+section .data
+	msg: db "Hello, Holberton", 0
+	fmt: db "%s", 10, 0
