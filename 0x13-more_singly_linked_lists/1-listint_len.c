d604f7cea7c5adc80e43ad1d875b23c7d48883b1
SamuelAmihere <amisam2000@gmail.com>
2023-04-29 04:34:01 -0700

    add file

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * listint_len - Finds number of elements in a linked
+ *
+ * @h: struct whose elements to be print elememts
+ *
+ * Return: number of elements in a linked
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t count = 0;
+
+	while (h)
+	{
+		count += 1;
+		h = h->next;
+	}
+
+	return (count);
+}
