2464cd61143f91c1409af783cf695463b49c74a5
SamuelAmihere <amisam2000@gmail.com>
2023-04-30 05:54:06 -0700

    correct error

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -18,8 +18,8 @@ void free_listint2(listint_t **head)
 		while (*head)
 		{
 			temp = *head;
-			free(*head);
-			*head = (*temp)->next;
+			*head = (*head)->next;
+			free(*temp);
 
 		}
 		*head = NULL;
