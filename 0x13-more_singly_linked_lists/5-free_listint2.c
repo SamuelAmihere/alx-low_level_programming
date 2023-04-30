b1e08f80c84b1a1fe6cee20fb2ed27e8eecf2c41
SamuelAmihere <amisam2000@gmail.com>
2023-04-30 03:47:23 -0700

    add file

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a list
+ *
+ * @head: struct freed
+ *
+ * Return: nothing
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	temp = *head;
+	if (*head)
+	{
+		while (*head)
+		{
+			temp = *head;
+			*head = (*head)->next;
+
+			free(temp);
+		}
+	}
+}
