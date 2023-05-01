88cbf95dc43bd7d6490bd0ba117e38bd94cbaa24
SamuelAmihere <amisam2000@gmail.com>
2023-05-01 03:18:49 -0700

    add file

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+#include <stdlib.h>
+
+int add_all(listint_t *head);
+
+/**
+ * sum_listint - Finds sum of elements in a list
+ *
+ * @head: Starting node
+ *
+ * Return: Sum of elements in list, 0 if empty
+ */
+int sum_listint(listint_t *head)
+{
+	return (add_all(head));
+}
+
+/**
+ * add_all - Adds all elements recursively
+ *
+ * @head: Starting node
+ *
+ * Return: Sum of elements in list, 0 if empty
+ */
+int add_all(listint_t *head)
+{
+	if (!head)
+		return head->n + add_all(head->next)
+	return (0);
+}
