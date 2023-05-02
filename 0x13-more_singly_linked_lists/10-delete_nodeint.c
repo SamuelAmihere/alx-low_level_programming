d4b28fba0b069c6f6d58a32ebd1e7960f063c927
SamuelAmihere <amisam2000@gmail.com>
2023-05-02 08:41:50 -0700

    correct error

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -29,7 +29,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	}
 
 	curr = (*head)->next;
-	while (curr && i != (index - 1))
+	while (curr && i < (index - 1))
 	{
 		prev = curr;
 		curr = curr->next;
@@ -37,7 +37,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	}
 
 
-	if (curr)
+	if (curr && i == index - 1)
 	{
 		temp = curr;
 		prev->next = curr->next;
