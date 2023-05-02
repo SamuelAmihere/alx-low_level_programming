57d20750a4a09634d6c2fb8673331ae8f8d3ee5e
SamuelAmihere <amisam2000@gmail.com>
2023-05-02 07:10:08 -0700

    correct error

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -20,7 +20,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 
 	if (head == 0 || *head == NULL || index > listint_len(*head))
 		return (-1);
-	else if (index == 0)
+	if (index == 0)
 	{
 		prev = *head;
 		*head = (*head)->next;
