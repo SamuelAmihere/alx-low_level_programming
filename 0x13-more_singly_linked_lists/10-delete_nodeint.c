1c27564e6bb749a66c1a57851c6c10d3cc4b570c
SamuelAmihere <amisam2000@gmail.com>
2023-05-02 01:01:47 -0700

    add file

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,65 @@
+#include "lists.h"
+#include <stdlib.h>
+
+size_t listint_len(const listint_t *h);
+
+/**
+ * delete_nodeint_at_index - Inserts a new node at a given
+ * position
+ *
+ * @head: Starting node
+ *
+ * @index: location to insert node
+ *
+ * Return: 1 on success, -1 on failure
+ */
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *temp, *prev, *curr;
+	unsigned int i = 0;
+
+	if (*head || idx > listint_len(*head))
+		return (-1);
+
+	prev = *head;
+	curr = (*head)->next;
+
+	while (curr && i < idx)
+	{
+		prev = curr;
+		curr = curr->next;
+		i += 1;
+	}
+
+
+	if (curr)
+	{
+		temp = curr;
+		prev->next = curr->next;
+		free(temp);
+		return (1);
+	}
+
+	return (-1);
+}
+
+
+/**
+ * listint_len - Finds number of elements in a linked
+ *
+ * @h: struct whose elements to be print elememts
+ *
+ * Return: number of elements in a linked
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t count = 0;
+
+	while (h)
+	{
+		count += 1;
+		h = h->next;
+	}
+
+	return (count);
+}
