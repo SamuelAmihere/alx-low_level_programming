9cd8c44806d4a52ebb7a8f3d99662187e79ffc23
SamuelAmihere <amisam2000@gmail.com>
2023-05-02 06:19:41 -0700

    correct error

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -49,3 +49,23 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	return (-1);
 
 }
+
+/**
+ * listint_len - Finds number of elements in a linked
+ *
+ * @h: struct whose elements to be print elememts
+ *
+ * Return: number of elements in a linked
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t count = 0;
+
+	while (h)
+	{
+		count += 1;
+		h = h->next;
+	}
+
+	return (count);
+}
