f614165b57e85604b54cc89e26946a6f94d839af
SamuelAmihere <amisam2000@gmail.com>
2023-05-02 06:16:50 -0700

    correct error

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,6 +1,7 @@
 #include "lists.h"
 #include <stdlib.h>
 
+size_t listint_len(const listint_t *h);
 
 /**
  * delete_nodeint_at_index - Inserts a new node at a given
@@ -17,7 +18,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	listint_t *temp, *prev, *curr;
 	unsigned int i = 0;
 
-	if (index == 0 && *head == NULL)
+	if ((index == 0 && *head == NULL) || index > listint_len(*head))
 		return (-1);
 	else if (index == 0 && (*head))
 	{
