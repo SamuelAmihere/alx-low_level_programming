b7d65cb8e4a091fc4c39392e9f466a14dbaf782b
SamuelAmihere <amisam2000@gmail.com>
2023-05-01 23:45:18 -0700

    correct error

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -22,21 +22,16 @@ listint_t *insert_nodeint_at_index(listint_t **head,
 
 	new = malloc(sizeof(listint_t));
 
-	if (!new)
+	if (!new || idx > listint_len(*head))
+	{
 		return (NULL);
+	}
 
 	new->n = n;
 	new->next = NULL;
 
 	prev = NULL;
 	temp = *head;
-
-	if (i == 0)
-	{
-		new->next = *head;
-		*head = new;
-		return (new);
-	}
 
 	while (temp && i < idx)
 	{
