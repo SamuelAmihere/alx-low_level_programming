c6d7e7e2c195af19ef5d68ec107bcb755aaeb8d2
SamuelAmihere <amisam2000@gmail.com>
2023-05-02 19:14:53 -0700

    correct error

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,6 +1,8 @@
 #include "lists.h"
 #include <stdlib.h>
 
+void free_listnode(listnode_t *head);
+
 /**
  * print_listint_safe - prints a listint_t linked list
  *
@@ -26,10 +28,25 @@ size_t print_listint_safe(const listint_t *h)
 		if (temp < curr)
 		{
 			printf("-> [%p] %d\n", (void *)curr, curr->n);
+			free(tem);
 			break;
 		}
 
 	}
 
+	free_listnode(curr);
+
 	return (count);
+}
+
+/**
+ * free_listnode - frees a free_listnode list
+ * @head: pointer to first node of the list
+ */
+void free_listnode(listnode_t *head)
+{
+	if (head == NULL)
+		return;
+	free_listnode(head->next);
+	free(head);
 }
