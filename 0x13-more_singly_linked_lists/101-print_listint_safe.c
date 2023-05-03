e91be9cf5fec0d3260354915f1cc125f02ed284f
SamuelAmihere <amisam2000@gmail.com>
2023-05-02 19:17:25 -0700

    correct error

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -34,19 +34,6 @@ size_t print_listint_safe(const listint_t *h)
 
 	}
 
-	free_listnode(temp);
 
 	return (count);
-}
-
-/**
- * free_listnode - frees a free_listnode list
- * @head: pointer to first node of the list
- */
-void free_listnode(listnode_t *head)
-{
-	if (head == NULL)
-		return;
-	free_listnode(head->next);
-	free(head);
 }
