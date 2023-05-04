60ee20076bdaee71004791ab3b572778b8e8b664
SamuelAmihere <amisam2000@gmail.com>
2023-05-03 21:03:02 -0700

    correct error

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -7,12 +7,12 @@
  *
  * Return: number of nodes in the list.
  */
-size_t print_listint_safe(const listint_t *h)
+size_t print_listint_safe(const listint_t *head)
 {
 	size_t cnt = 0;
-	const listint_t *temp = h;
+	const listint_t *temp = head;
 
-	if (!h)
+	if (!head)
 		exit(98);
 
 	while (temp)
