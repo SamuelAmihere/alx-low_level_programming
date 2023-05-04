89b7941e5d8df5dcd3f2a306a8d82c4c8ed62bef
SamuelAmihere <amisam2000@gmail.com>
2023-05-03 20:17:20 -0700

    correct error

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,6 +1,5 @@
 #include "lists.h"
-#include <stdlib.h>
-
+#include <stdio.h>
 
 /**
  * print_listint_safe - prints a listint_t linked list
@@ -11,94 +10,28 @@
  */
 size_t print_listint_safe(const listint_t *h)
 {
-	const listint_t *loop_node = find_listint_loop(head);
+	const listint_t *curr;
+	size_t cnt;
 
-	if (loop_node == NULL)
-		return (print_listint(head));
+	cnt = 0;
+	curr = h;
+	if (h == NULL)
+		return (cnt);
 
-	return (print_listint_loop(head, loop_node));
-}
-
-/**
- * find_listint_loop - finds a loop in a listint_t linked list
- *
- *  @head: pointer to the first node in the list
- *
- *  Return: pointer to the node where the loop starts, or NULL if no loop
- */
-const listint_t *find_listint_loop(const listint_t *head)
-{
-	const listint_t *slow_p = head, *fast_p = head;
-
-	while (slow_p && fast_p && fast_p->next)
+	while (curr)
 	{
-		slow_p = slow_p->next;
-		fast_p = fast_p->next->next;
-		if (slow_p == fast_p)
+		printf("[%p] %d\n", (void *)curr, curr->n);
+		cnt += 1;
+
+		if ((curr->next) >= curr)
 		{
-			slow_p = head;
-			while (slow_p != fast_p)
-			{
-				slow_p = slow_p->next;
-				fast_p = fast_p->next;
-			}
-			return (slow_p);
+			printf("-> [%p] %d\n", (void *)curr->next,
+					(curr->next)->n);
+			return (cnt);
 		}
-	}
-	return (NULL);
-}
 
-/**
- * print_listint - prints a listint_t linked list from a given head
- *
- * @head: pointer to the first node in the list
- *
- * Return: number of nodes in the list
- */
-size_t print_listint(const listint_t *head)
-{
-	size_t cnt = 0;
+		curr = curr->next;
 
-	while (head)
-	{
-		printf("[%p] %d\n", (void *)head, head->n);
-		cnt += 1;
-		head = head->next;
-	}
-	return (cnt);
-}
-
-/**
- * print_listint_loop - prints a listint_t linked list with a loop
- *
- * @head: pointer to the first node in the list
- *
- * @loop_node: pointer to the node where the loop starts
- *
- * Return: number of nodes in the list before the loop
- */
-size_t print_listint_loop(const listint_t *head, const listint_t *loop_node)
-{
-	size_t cnt = 0;
-
-	while (head != loop_node)
-	{
-		printf("[%p] %d\n", (void *)head, head->n);
-		cnt += 1;
-		head = head->next;
-	}
-
-	printf("[%p] %d\n", (void *)loop_node, loop_node->n);
-	printf("-> [%p] %d\n", (void *)loop_node->next, loop_node->next->n);
-
-	cnt += 1;
-
-	head = loop_node->next;
-	while (head != loop_node)
-	{
-		printf("[%p] %d\n", (void *)head, head->n);
-		cnt += 1;
-		head = head->next;
 	}
 
 	return (cnt);
