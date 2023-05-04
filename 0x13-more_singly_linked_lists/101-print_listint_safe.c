e913bf8feefbe86aa93b9c316367296fec57092c
SamuelAmihere <amisam2000@gmail.com>
2023-05-03 20:41:39 -0700

    correct error

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -2,52 +2,55 @@
 #include <stdio.h>
 
 /**
- * loop_check - function that checks if a linked list contains a loop.
- *
+ * print_listint_safe - function that prints a listint_t linked list.
  * @head: pointer to head of a list.
  *
- * Return: 1 if loop is present, 0 if not.
+ * Return: Length of list (INT)
  */
-int loop_check(const listint_t *head)
+size_t print_listint_safe(const listint_t *head)
 {
 	const listint_t *slow_p = head, *fast_p = head;
+	size_t ele = 0;
+	int is_loop = 0;
 
 	while (slow_p && fast_p && fast_p->next)
 	{
+		if (!(fast_p->next->next))
+			break;
 		slow_p = slow_p->next;
 		fast_p = fast_p->next->next;
 		if (slow_p == fast_p)
 		{
+			slow_p = slow_p->next;
+			is_loop = 1;
+			break;
+		}
+	}
+
+	if (!is_loop)
+	{
+		while (head)
+		{
+			ele++;
+			printf("[%p] %d\n", (void *)head, head->n);
+			head = head->next;
+		}
+		return (ele);
+	}
+
+	while (head)
+	{
+		ele++;
+		if (head == slow_p)
+		{
 			printf("[%p] %d\n", (void *)head, head->n);
 			printf("-> [%p] %d\n", (void *)head, head->next->n);
-			return (1);
+			exit(98);
 		}
+
+		printf("[%p] %d\n", (void *)head, head->n);
+		head = head->next;
 	}
 	return (0);
 }
 
-/**
- * print_listint_safe - prints a listint_t linked list
- *
- * @h: pointer first node in list
- *
- * Return: Number of nodes in the list
- */
-size_t print_listint_safe(const listint_t *h)
-{
-	size_t ele = 0;
-
-	if (!h)
-		return (0);
-
-	if (loop_check(h))
-		return (0);
-
-	while (h)
-	{
-		cnt++;
-		printf("[%p] %d\n", (void *)h, h->n);
-		h = h->next;
-	}
-	return (cnt);
-}
