1a0246cc24d0b4fb2297345da675b41906080942
SamuelAmihere <amisam2000@gmail.com>
2023-05-03 20:32:36 -0700

    correct error

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -2,6 +2,31 @@
 #include <stdio.h>
 
 /**
+ * loop_check - function that checks if a linked list contains a loop.
+ *
+ * @head: pointer to head of a list.
+ *
+ * Return: 1 if loop is present, 0 if not.
+ */
+int loop_check(const listint_t *head)
+{
+	const listint_t *slow_p = head, *fast_p = head;
+
+	while (slow_p && fast_p && fast_p->next)
+	{
+		slow_p = slow_p->next;
+		fast_p = fast_p->next->next;
+		if (slow_p == fast_p)
+		{
+			printf("[%p] %d\n", (void *)head, head->n);
+			printf("-> [%p] %d\n", (void *)head, head->next->n);
+			return (1);
+		}
+	}
+	return (0);
+}
+
+/**
  * print_listint_safe - prints a listint_t linked list
  *
  * @h: pointer first node in list
@@ -10,29 +35,19 @@
  */
 size_t print_listint_safe(const listint_t *h)
 {
-	const listint_t *curr;
-	size_t cnt;
+	size_t ele = 0;
 
-	cnt = 0;
-	curr = h;
-	if (h == NULL)
-		return (cnt);
+	if (!h)
+		return (0);
 
-	while (curr)
+	if (loop_check(h))
+		return (0);
+
+	while (h)
 	{
-		printf("[%p] %d\n", (void *)curr, curr->n);
-		cnt += 1;
-
-		if ((curr->next) >= curr)
-		{
-			printf("-> [%p] %d\n", (void *)curr->next,
-					(curr->next)->n);
-			return (cnt);
-		}
-
-		curr = curr->next;
-
+		cnt++;
+		printf("[%p] %d\n", (void *)h, h->n);
+		h = h->next;
 	}
-
 	return (cnt);
 }
