c896a4c023ae368a6268046422d7056a3778edc6
SamuelAmihere <amisam2000@gmail.com>
2023-05-04 22:16:20 -0700

    correct error

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -11,7 +11,7 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	if (index > U_BITS(sizeof(unsigned long int)))
+	if (index - 1 > U_BITS(sizeof(unsigned long int)))
 		return (-1);
 
 	return ((n >> index) & 1);
