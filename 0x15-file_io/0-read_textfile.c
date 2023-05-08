b304bf277dca0f837da08c2e9a9c4a3c110a406c
SamuelAmihere <amisam2000@gmail.com>
2023-05-08 15:47:10 -0700

    correct error

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,10 +1,10 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include "main.h"
 
 /**
  * read_textfile - reads a text file and prints it
