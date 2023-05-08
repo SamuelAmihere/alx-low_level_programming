2e3f96ae02cc2ea8cafd966c6bd88ce2e0bf3863
SamuelAmihere <amisam2000@gmail.com>
2023-05-08 16:18:18 -0700

    add file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -22,7 +22,7 @@ int create_file(const char *filename, char *text_content)
 {
 	int fdisc, len, bytes_w;
 
-	fdisc = open(filename, O_WEONLY | O_CREAT | O_TRUNC, 0600);
+	fdisc = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 
 	if (!filename || fdisc == -1)
 		return (-1);
