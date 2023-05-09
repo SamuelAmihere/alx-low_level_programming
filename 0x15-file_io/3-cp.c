8b8cddc61a7219570c4f244a2f43131cca3d9dc2
SamuelAmihere <amisam2000@gmail.com>
2023-05-08 20:17:21 -0700

    correct error

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -85,7 +85,8 @@ void copy_file(char *sr, char *dst)
 	int bytes_r, bytes_w, src, dest;
 
 	src = open_file(sr, O_RDONLY);
-	dest = open_file(dst, O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	dest = open_file(dst, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR |
+			S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
 
 
 	while ((bytes_r = read(src, buffer, BUFFER_SIZE)) > 0)
