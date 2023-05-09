e568f6749c1c031f9f17104e674bc925c27e37e1
SamuelAmihere <amisam2000@gmail.com>
2023-05-08 17:24:44 -0700

    correct error

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,115 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+#define BUFFER_SIZE 1024
+
+/**
+ * error - print an error message and exit with a given error code
+ *
+ * @message: error message to output
+ *
+ * @code: exit code
+ *
+ * Return: nothing
+ */
+void error(char *message, int code)
+{
+	dprintf(STDERR_FILENO, "Error: %s\n", message);
+	exit(code);
+}
+
+/**
+ * open_file - open a file
+ *
+ * @filename: file name
+ * @flags: flag for file permissions
+ * @mode: file mode
+ *
+ * Return: file descriptor
+ */
+int open_file(char *filename, int flags, mode_t mode)
+{
+	int fdesc;
+
+	fdesc = open(filename, flags, mode);
+	if (fdesc < 0)
+	{
+		error(strerror(errno), 98);
+	}
+	return (fdesc);
+}
+
+/**
+ * copy_file - read from one file and write to another
+ *
+ * @src: source file
+ * @dest: destination file
+ *
+ * Return: nothing
+ */
+void copy_file(int src, int dest)
+{
+	char buffer[BUFFER_SIZE];
+	ssize_t bytes_r, bytes_w;
+
+	while ((bytes_r = read(src, buffer, BUFFER_SIZE)) > 0)
+	{
+		bytes_w = write(dest, buffer, bytes_r);
+		if (bytes_w < 0)
+			error(strerror(errno), 99);
+	}
+
+	if (bytes_r < 0)
+		error(strerror(errno), 98);
+}
+
+
+/**
+ * close_file - close a file
+ *
+ * @fdesc: file descriptor
+ *
+ * Return: nothing
+ */
+void close_file(int fdesc)
+{
+	if (close(fdesc)
+		error(strerror(errno), 100);
+}
+
+
+/**
+ * main - copies a file's contents to another file
+ *
+ * @argc: number of parameters
+ *
+ * @argv: pointer to arguments
+ *
+ * Return: o on Success
+ * exit code 97 (incorrect argc)
+ * exit code 98 (src file does not exist)
+ * exit code 99 (dest file does not exist)
+ * exit code 100 (both files connot be closed)
+ */
+int main(int argc, char *argv[])
+{
+	int src, dest;
+
+	if (argc != 3)
+		error("Usage: cp file_from file_to", 97);
+
+	src = open_file(argv[1], O_RDONLY, 0);
+	dest = open_file(argv[2], O_WRONLY | O_CREAT | O_TRUNC,
+			S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+
+	copy_file(src, dest);
+
+	close_file(src);
+	close_file(dest);
+
+	return (0);
+}
