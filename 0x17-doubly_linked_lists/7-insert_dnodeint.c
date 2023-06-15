0dd5f5465c7775008ae492ca4b2e3208d93a857d
SamuelAmihere <amisam2000@gmail.com>
2023-06-15 15:44:01 -0700

    first fix bug

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -22,7 +22,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **head,
 
 	new = malloc(sizeof(dlistint_t));
 
-	if (!new || idx > dlistint_len(*head))
+	if (!new || idx >= dlistint_len(*head))
 		return (NULL);
 	if (idx == 0)
 	{
