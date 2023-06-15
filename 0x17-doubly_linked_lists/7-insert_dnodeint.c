50aacdf254e09fcb6280401f71af7d4562d5de23
SamuelAmihere <amisam2000@gmail.com>
2023-06-15 15:17:58 -0700

    first commit

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -46,3 +46,23 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **head,
 	}
 	return (new);
 }
+
+/**
+ * dlistint_len - Finds number of elements in a dlinked list
+ *
+ * @h: struct whose elements to be print elememts
+ *
+ * Return: number of elements in a linked
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t count = 0;
+
+	while (h)
+	{
+		count += 1;
+		h = h->next;
+	}
+
+	return (count);
+}
