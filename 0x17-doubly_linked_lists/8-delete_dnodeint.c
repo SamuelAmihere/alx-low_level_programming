216dd43873fcff5cf248b3888ec6ea0c0f987791
SamuelAmihere <amisam2000@gmail.com>
2023-06-16 09:36:23 -0700

    fix bug

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -20,6 +20,18 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	if (*head == NULL || head == NULL)
 		return (-1);
 
+	prev = *head;
+	if (index == 0)
+	{
+		*head = (*head)->next;
+		if (!(*head))
+		{
+			return (1);
+		}
+		(*head)->prev = NULL;
+		free(prev);
+		return (1);
+	}
 	curr = (*head)->next;
 	while (curr && i < (index - 1))
 	{
