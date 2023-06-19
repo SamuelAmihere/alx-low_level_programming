519d50c0bef39f73bb8c0a547ea000d2c944fd6a
SamuelAmihere <amisam2000@gmail.com>
2023-06-19 10:19:08 -0700

    add 101-make_me_win

---
+++ 0x18-dynamic_libraries/generator.c
@@ -0,0 +1,25 @@
+#include <string.h>
+#include <unistd.h>
+
+int rand()
+{
+	static int tracker = -1;
+
+	tracker++;
+	switch (tracker)
+	{
+		case 0:
+		case 1:
+			return 8;
+		case 2:
+			return 7;
+		case 3:
+			return 9;
+		case 4:
+			return 23;
+		case 5:
+			return 74;
+		default:
+			return tracker * tracker % 30000;
+	}
+}
