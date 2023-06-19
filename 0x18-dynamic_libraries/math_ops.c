f3d661cbc468331ab1fec562aa64096ef77efc37
SamuelAmihere <amisam2000@gmail.com>
2023-06-19 07:05:41 -0700

    add 100-operations

---
+++ 0x18-dynamic_libraries/math_ops.c
@@ -0,0 +1,66 @@
+#include "math_ops.h"
+
+/**
+ * add - performs addition
+ *
+ * @a: first number
+ * @b: second number
+ *
+ * Return: results
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - performs subtraction
+ *
+ * @a: first number
+ * @b: second number
+ *
+ * Return: results
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - performs multiplication
+ *
+ * @a: first number
+ * @b: second number
+ *
+ * Return: results
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - performs division
+ *
+ * @a: first number
+ * @b: second number
+ *
+ * Return: results
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - performs modulo
+ *
+ * @a: first number
+ * @b: second number
+ *
+ * Return: results
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
