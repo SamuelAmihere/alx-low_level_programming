c32e02b83ed56966b602e5b2a42608630d562c4d
SamuelAmihere <amisam2000@gmail.com>
2023-06-20 10:35:09 -0700

    fix bug

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-wget -P .. https://github.com/SamuelAmihere/alx-low_level_programming/blob/master/0x18-dynamic_libraries/libgenerator.so
-export LD_PRELOAD="$PWD/../libgenerator.so"
+unset LD_PRELOAD
