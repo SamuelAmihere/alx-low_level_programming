aac0cf7c2b505a8255b9c57687d62191419fcaf7
SamuelAmihere <amisam2000@gmail.com>
2023-06-20 09:54:16 -0700

    fix bug

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-wget -P https://github.com/SamuelAmihere/alx-low_level_programming/blob/master/0x18-dynamic_libraries/generator.o
-gcc -fPIC -shared *.o -0 libgenerator.o
+gcc -shared *.o -0 libgenerator.so
