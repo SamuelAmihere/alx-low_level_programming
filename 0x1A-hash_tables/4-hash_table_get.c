ad4b2f4684d70d77b0a6019e811aa8d2f268cb03
SamuelAmihere <amisam2000@gmail.com>
2023-07-07 07:16:15 -0700

    fix bug

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -16,7 +16,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	if (!key || !ht)
 		return (NULL);
 
-	index = key_index((unsigned char *) key, ht->size);
+	index = key_index((const unsigned char *) key, ht->size);
 	node = ht->array[index];
 
 	if (!node)
