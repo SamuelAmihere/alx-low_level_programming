9326eb895a0c79025a19a7a0fca0dfbddfa75b62
SamuelAmihere <amisam2000@gmail.com>
2023-07-06 17:36:24 -0700

    add 2-key_index: gives you the index of a key

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,14 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - Gives index of a key in hash table
+ *
+ * @key: the key to retrive
+ * @size: size of the array of the has table
+ *
+ * Return: index of a key
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	return (hash_djb2(key) % size);
+}
