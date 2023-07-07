046f3c2ad59f9e3cfd43ff279cf336ca783d2ff1
SamuelAmihere <amisam2000@gmail.com>
2023-07-07 07:13:15 -0700

    add 6-hash_table_delete

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -9,8 +9,24 @@
  */
 void hash_table_delete(hash_table_t *ht)
 {
-	hash_node_t *tmp = NULL, *node = NULL;
+	hash_node_t *node = NULL, *tmp = NULL;
 	unsigned long int i = 0;
 
+	if (!ht)
+		return;
 
+	for (i = 0; i < ht->size; i++)
+	{
+		node = ht->array[i];
+		while (node)
+		{
+			tmp = node;
+			node = node->next;
+			free(tmp->key);
+			free(tmp->value);
+			free(tmp);
+		}
+	}
+	free(ht->array);
+	free(ht);
 }
