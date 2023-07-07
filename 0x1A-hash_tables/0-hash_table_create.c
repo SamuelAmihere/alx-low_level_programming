da12d081103ee0f76ed0d691b0d03996b59e1561
SamuelAmihere <amisam2000@gmail.com>
2023-07-06 17:12:38 -0700

    fix bug

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -16,11 +16,11 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (!table || !size)
 		return (NULL);
 
-	table->size = size;
 	table->array = malloc(sizeof(hash_node_t) * size);
 	if (!(table->array))
 		return (NULL);
 
+	table->size = size;
 	for (i = 0 ; i < size; i++)
 		table->array[i] = NULL;
 
