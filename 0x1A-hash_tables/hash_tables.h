50c8b86b800aec0c85fae1467c84a801b0b6c851
SamuelAmihere <amisam2000@gmail.com>
2023-07-07 17:12:25 -0700

    add 100-sorted_hash_table

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -90,4 +90,6 @@ void shash_table_print(const shash_table_t *ht);
 void shash_table_print_rev(const shash_table_t *ht);
 void shash_table_delete(shash_table_t *ht);
 
+/*helpers*/
+void insert_sorted_node(shash_table_t *ht, shash_node_t *new_node);
 #endif
