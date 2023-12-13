01c8a2348f35ba0cbf7db08f5ea30f571cf9f9af
Samuel Amihere <amisam2000@gmail.com>
2023-12-14 00:15:06 +0800

    search algos

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -9,18 +9,18 @@
  */
 int linear_search(int *array, size_t size, int value)
 {
-	unsigned int i = 0;
+	int i = 0;
 
 
-	if (array == NULL)
+	if (!array)
 		return (-1);
 
-	for (i = 0; i < size; i++)
+	for (i = 0; i < (int)size; i++)
 	{
 		printf("Value checked array[%d] = [%d]\n", i, array[i]);
-
 		if (array[i] == value)
 			return (i);
 	}
+
 	return (-1);
 }
